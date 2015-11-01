b7fe9e21260f25dce60218d6442313f25b14856e
palvarad <palvaradgh@gmail.com>
2015-11-01 17:39:49 -0500

    Comments and clean up.

--- animalShelterManager/ShelterPerson.java
+++ animalShelterManager/ShelterPerson.java
@@ -18,7 +18,7 @@ public abstract class ShelterPerson {
 	}
 	
 	/**
-	 * Accessor method to return the first name of the manager.
+	 * Accessor method to return the first name.
 	 * @return String firstName
 	 */
 	public String getFirstName(){
@@ -26,7 +26,7 @@ public abstract class ShelterPerson {
 	}
 	
 	/**
-	 * Accessor method to return the last name of the manager.
+	 * Accessor method to return the last name.
 	 * @return
 	 */
 	public String getLastName(){
@@ -34,11 +34,11 @@ public abstract class ShelterPerson {
 	}
 	
 	/**
-	 * Mutator method to set the first name of the manager. If the name is blank an exception is thrown.
+	 * Mutator method to set the first name. If the name is blank an exception is thrown.
 	 * @param String fName
 	 * @return boolean
 	 */
-	public boolean setFirstName(String fName){
+	public void setFirstName(String fName){
 		//If the name is blank then an exception is thrown to inform the user.
 		if(fName.trim().equals("")){
 			throw new IllegalArgumentException("The first name cannot be blank");
@@ -46,16 +46,15 @@ public abstract class ShelterPerson {
 		//If the name is not blank then the name is stored and a true is return.
 		else{
 			this.firstName = fName;
-			return true;
 		}
 	}
 	
 	/**
-	 * Mutator method to set the last name of the manager. If the name is blank an exception is thrown.
+	 * Mutator method to set the last name. If the name is blank an exception is thrown.
 	 * @param lName
 	 * @return boolean
 	 */
-	public boolean setLastName(String lName){
+	public void setLastName(String lName){
 		//If the name is blank then an exception is thrown to inform the user.
 		if(lName.trim().equals("")){
 			throw new IllegalArgumentException("The last name cannot be blank");
@@ -63,12 +62,11 @@ public abstract class ShelterPerson {
 		//If the name is not blank then the name is stored and a true is return.
 		else{
 			this.lastName = lName;
-			return true;
 		}
 	}
 	
 	/**
-	 * toString method to return the first name and last name of the manager.
+	 * toString method to return the first name and last name.
 	 * @return String managerInfo
 	 */
 	public String toString(){
