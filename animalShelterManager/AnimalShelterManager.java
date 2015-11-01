4de512f35d6f0942a3e561671d5729c739ba0621
palvarad <palvaradgh@gmail.com>
2015-11-01 14:34:52 -0500

    Attempt at using one method to read all files.

--- animalShelterManager/AnimalShelterManager.java
+++ animalShelterManager/AnimalShelterManager.java
@@ -11,11 +11,13 @@ public class AnimalShelterManager {
 
 	public static void main(String[] args) {
 		final String TITLE = "Animal Shelter Manager";
+		final String[] LOGIN_OPTIONS = {"Manager", "Employee"};
+		final String LOGIN_PROMPT = "Select User Type:";
 		
 		Manager shelterManager = new Manager();
-		createManager(shelterManager);
+		readFile(shelterManager);
 		
-		int choice = JOptionPane.showConfirmDialog(null, "Sign in as a manager?", TITLE, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null);
+		int choice = JOptionPane.showOptionDialog(null, LOGIN_PROMPT, TITLE, 0, JOptionPane.QUESTION_MESSAGE, null, LOGIN_OPTIONS, null);
 		if(choice == 0){
 			//Example how to use the exceptions being thrown by the DDCs.
 			try{
@@ -30,21 +32,34 @@ public class AnimalShelterManager {
 
 	}
 	
-	private static void createManager(Manager currentManager){
+	private static void readFile(Object shelterObject){
 		//String for the title of all the messages for this method
 		final String TITLE = "Animal Shelter Manager";
 		//Variable to store the last position of the semicolon used for dividing attributes.
-		int lastInfoDiv = 0;
+		int lastInfoDiv;
 		//Variable to store the position where to start to search for the next element.
-		int divPosition = 0;
+		int divPosition;
+		//Scanner object to read the file set to null.
+		Scanner reader = null;
+		//String to store the file path. Each object type has its own file path.
+		String filePath = null;
+		
+		Manager currentManager = null;
+		
+		if(shelterObject instanceof Manager){
+			currentManager = (Manager)shelterObject;
+			filePath = Manager.toFile();
+		}
 		
 		//Try and catch to prevent the program from crashing if the file is missing or other I/O errors.
 		try{
 			//Scanner object to read the file information
-			Scanner reader = new Scanner(new FileInputStream(Manager.toFile()));
+			reader = new Scanner(new FileInputStream(filePath));
 			
 			//While loop to iterate until there are no more lines to read from the file.
 			while(reader.hasNextLine()){
+				lastInfoDiv = 0;
+				divPosition = 0;
 				//String to hold a line from the file.
 				String line = reader.nextLine();
 				//Finds the first semicolon and cuts the unnecessary parts to only have the first name.
